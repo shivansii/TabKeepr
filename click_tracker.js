b6ffa57ea35564e13810e9b09b07af2569933fb7
Shivansi <shivansikp@gmail.com>
2023-01-26 11:42:38 -0600

    first commit

---
+++ click_tracker.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var startButton = document.getElementById('start');
+    var stopButton = document.getElementById('stop');
+  
+    startButton.addEventListener('click', function() {
+      // Start recording clicks
+    });
+  
+    stopButton.addEventListener('click', function() {
+      // Stop recording clicks
+    });
+  });
+  
